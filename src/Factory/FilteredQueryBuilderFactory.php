85479e3202df685cbefee66973a9099a12558e90
Adrien Crivelli <adrien.crivelli@gmail.com>
2018-06-12 17:34:39 +0900

    Simplify code

--- src/Factory/FilteredQueryBuilderFactory.php
+++ src/Factory/FilteredQueryBuilderFactory.php
@@ -27,6 +27,11 @@ final class FilteredQueryBuilderFactory extends AbstractFactory
      */
     private $sortingTypeFactory;
 
+    /**
+     * @var QueryBuilder
+     */
+    private $queryBuilder;
+
     public function __construct(Types $types, EntityManager $entityManager, SortingTypeFactory $sortingTypeFactory)
     {
         parent::__construct($types, $entityManager);
@@ -38,14 +43,14 @@ final class FilteredQueryBuilderFactory extends AbstractFactory
         $this->uniqueNameFactory = new UniqueNameFactory();
         $alias = $this->uniqueNameFactory->createAliasName($className);
 
-        $queryBuilder = $this->entityManager->getRepository($className)->createQueryBuilder($alias);
+        $this->queryBuilder = $this->entityManager->getRepository($className)->createQueryBuilder($alias);
         $metadata = $this->entityManager->getClassMetadata($className);
         $type = $this->types->getFilter($className);
 
-        $this->applyJoinsAndFilters($metadata, $type, $filter, $queryBuilder, $alias);
-        $this->applySorting($className, $sorting, $queryBuilder, $alias);
+        $this->applyJoinsAndFilters($metadata, $type, $filter, $alias);
+        $this->applySorting($className, $sorting, $alias);
 
-        return $queryBuilder;
+        return $this->queryBuilder;
     }
 
     /**
@@ -54,13 +59,12 @@ final class FilteredQueryBuilderFactory extends AbstractFactory
      * @param ClassMetadata $metadata
      * @param InputObjectType $type
      * @param array $filter
-     * @param QueryBuilder $queryBuilder
      * @param string $alias
      */
-    private function applyJoinsAndFilters(ClassMetadata $metadata, InputObjectType $type, array $filter, QueryBuilder $queryBuilder, string $alias): void
+    private function applyJoinsAndFilters(ClassMetadata $metadata, InputObjectType $type, array $filter, string $alias): void
     {
-        $this->applyJoins($metadata, $filter, $queryBuilder, $alias);
-        $this->applyFilters($metadata, $type, $filter, $queryBuilder, $alias);
+        $this->applyJoins($metadata, $filter, $alias);
+        $this->applyFilters($metadata, $type, $filter, $alias);
     }
 
     /**
@@ -69,18 +73,17 @@ final class FilteredQueryBuilderFactory extends AbstractFactory
      * @param ClassMetadata $metadata
      * @param InputObjectType $type
      * @param array $filter
-     * @param QueryBuilder $queryBuilder
      * @param string $alias
      *
      * @throws \Exception
      */
-    private function applyFilters(ClassMetadata $metadata, InputObjectType $type, array $filter, QueryBuilder $queryBuilder, string $alias): void
+    private function applyFilters(ClassMetadata $metadata, InputObjectType $type, array $filter, string $alias): void
     {
         $typeFields = $type->getField('conditions')->type->getWrappedType(true)->getField('fields')->type->getWrappedType(true);
         foreach ($filter['conditions'] ?? [] as $conditions) {
-            $dqlConditions = $this->getDqlConditions($metadata, $conditions['fields'], $typeFields, $queryBuilder, $alias);
+            $dqlConditions = $this->getDqlConditions($metadata, $conditions['fields'], $typeFields, $alias);
 
-            $this->applyDqlConditions($queryBuilder, $conditions, $dqlConditions);
+            $this->applyDqlConditions($conditions, $dqlConditions);
         }
     }
 
@@ -90,12 +93,11 @@ final class FilteredQueryBuilderFactory extends AbstractFactory
      * @param ClassMetadata $metadata
      * @param array $allFields
      * @param InputObjectType $typeFields
-     * @param QueryBuilder $queryBuilder
      * @param string $alias
      *
      * @return array
      */
-    private function getDqlConditions(ClassMetadata $metadata, array $allFields, InputObjectType $typeFields, QueryBuilder $queryBuilder, string $alias): array
+    private function getDqlConditions(ClassMetadata $metadata, array $allFields, InputObjectType $typeFields, string $alias): array
     {
         $dqlConditions = [];
         foreach ($allFields as $fields) {
@@ -113,7 +115,7 @@ final class FilteredQueryBuilderFactory extends AbstractFactory
                     /** @var AbstractOperator $operatorType */
                     $operatorType = $operatorField->type;
 
-                    $condition = $operatorType->getDqlCondition($this->uniqueNameFactory, $metadata, $queryBuilder, $alias, $field, $operatorArgs);
+                    $condition = $operatorType->getDqlCondition($this->uniqueNameFactory, $metadata, $this->queryBuilder, $alias, $field, $operatorArgs);
                     if ($condition) {
                         $dqlConditions[] = $condition;
                     }
@@ -129,34 +131,33 @@ final class FilteredQueryBuilderFactory extends AbstractFactory
      *
      * @param ClassMetadata $metadata
      * @param array $filter
-     * @param QueryBuilder $queryBuilder
      * @param string $alias
      *
      * @throws \Doctrine\ORM\Mapping\MappingException
      */
-    private function applyJoins(ClassMetadata $metadata, array $filter, QueryBuilder $queryBuilder, string $alias): void
+    private function applyJoins(ClassMetadata $metadata, array $filter, string $alias): void
     {
         foreach ($filter['joins'] ?? [] as $field => $join) {
             $relationship = $alias . '.' . $field;
             $joinedAlias = $this->uniqueNameFactory->createAliasName($field);
 
             if ($join['type'] === 'innerJoin') {
-                $queryBuilder->innerJoin($relationship, $joinedAlias);
+                $this->queryBuilder->innerJoin($relationship, $joinedAlias);
             } else {
-                $queryBuilder->leftJoin($relationship, $joinedAlias);
+                $this->queryBuilder->leftJoin($relationship, $joinedAlias);
             }
 
             // TODO: For now we assume the query will always access some field on the relation, so we optimize SQL by
             // fetching those objects in a single SQL query. But this should be revisited by either exposing an option
             // to the API so the client could decide to select or not the relations, or even better to detect in GraphQL
             // query if it's actually used or not.
-            $queryBuilder->addSelect($joinedAlias);
+            $this->queryBuilder->addSelect($joinedAlias);
 
             if (isset($join['filter'])) {
                 $targetClassName = $metadata->getAssociationMapping($field)['targetEntity'];
                 $targetMetadata = $this->entityManager->getClassMetadata($targetClassName);
                 $type = $this->types->getFilter($targetClassName);
-                $this->applyJoinsAndFilters($targetMetadata, $type, $join['filter'], $queryBuilder, $joinedAlias);
+                $this->applyJoinsAndFilters($targetMetadata, $type, $join['filter'], $joinedAlias);
             }
         }
     }
@@ -166,17 +167,16 @@ final class FilteredQueryBuilderFactory extends AbstractFactory
      *
      * @param string $className
      * @param array $sorting
-     * @param QueryBuilder $queryBuilder
      * @param string $alias
      */
-    private function applySorting(string $className, array $sorting, QueryBuilder $queryBuilder, string $alias): void
+    private function applySorting(string $className, array $sorting, string $alias): void
     {
         foreach ($sorting as $sort) {
             $customSort = $this->sortingTypeFactory->getCustomSorting($className, $sort['field']);
             if ($customSort) {
-                $customSort($queryBuilder, $sort['order']);
+                $customSort($this->queryBuilder, $sort['order']);
             } else {
-                $queryBuilder->addOrderBy($alias . '.' . $sort['field'], $sort['order']);
+                $this->queryBuilder->addOrderBy($alias . '.' . $sort['field'], $sort['order']);
             }
         }
     }
@@ -184,26 +184,25 @@ final class FilteredQueryBuilderFactory extends AbstractFactory
     /**
      * Apply DQL conditions on the query builder
      *
-     * @param QueryBuilder $queryBuilder
      * @param array $conditions
      * @param array $dqlConditions
      */
-    private function applyDqlConditions(QueryBuilder $queryBuilder, array $conditions, array $dqlConditions): void
+    private function applyDqlConditions(array $conditions, array $dqlConditions): void
     {
         if (!$dqlConditions) {
             return;
         }
 
         if ($conditions['fieldsLogic'] === 'AND') {
-            $fieldsDql = $queryBuilder->expr()->andX(...$dqlConditions);
+            $fieldsDql = $this->queryBuilder->expr()->andX(...$dqlConditions);
         } else {
-            $fieldsDql = $queryBuilder->expr()->orX(...$dqlConditions);
+            $fieldsDql = $this->queryBuilder->expr()->orX(...$dqlConditions);
         }
 
         if ($conditions['conditionLogic'] === 'AND') {
-            $queryBuilder->andWhere($fieldsDql);
+            $this->queryBuilder->andWhere($fieldsDql);
         } else {
-            $queryBuilder->orWhere($fieldsDql);
+            $this->queryBuilder->orWhere($fieldsDql);
         }
     }
 }
